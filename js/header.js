170c3b9f144037cc916661dbe485f500ce913cab
Joshua Cole <j_cole_356@yahoo.com>
2017-11-01 11:33:42 -0700

    use media queries to modify header layout for mobile

--- js/header.js
+++ js/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MediaQuery from 'react-responsive';
 import Button from './button'
 
 export default class Header extends React.Component {
@@ -7,14 +8,35 @@ export default class Header extends React.Component {
         super(props)
     }
 
+    // TODO: add a media query to format the button
+    // TODO: stop adding the undefined class to some instances of the button
     render() {
         return (
-            <div className="header">
-                <Button
-                    active={true}
-                    onClick={this.props.addNote}
-                    text={'+ Add Note'}
-                />
+            <div>
+                <MediaQuery query="(max-device-width: 768px)">
+                    <div className="header">
+                        <div className="header-title">
+                            Notes
+                        </div>
+                        <Button
+                            active={true}
+                            onClick={this.props.addNote}
+                            text={'+'}
+                        />
+                    </div>
+                </MediaQuery>
+                <MediaQuery query="(min-device-width: 769px)">
+                    <div className="header">
+                        <div className="header-title">
+                            Notes
+                        </div>
+                        <Button
+                            active={true}
+                            onClick={this.props.addNote}
+                            text={'+ Add Note'}
+                        />
+                    </div>
+                </MediaQuery>
             </div>
         )
     }

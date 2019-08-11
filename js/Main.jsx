e69ea8216b15d8306a10bfe103dc10bef59aa7ce
Joshua Cole <joshuacole356@gmail.com>
2019-08-10 20:46:32 -0700

    add basic routing to Main and add login screen

--- js/Main.jsx
+++ js/Main.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Container from "./Container";
+import Login from "./Login";
 import "../css/main.css";
 
 function Page() {
-  return <Container />;
+  return (
+    <Router>
+      <Route exact path="/" component={Login} />
+      <Route path="/Container" component={Container} />
+    </Router>
+  );
 }
 
 // eslint-disable-next-line no-undef

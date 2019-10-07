7b499f0fded32e4a45687736243efc91ba15b11f
Joshua Cole <joshuacole356@gmail.com>
2019-10-06 23:39:19 -0700

    save user notes on server

--- js/services/api.js
+++ js/services/api.js
@@ -1,6 +1,8 @@
 const baseHeaders = () => {
   const myHeaders = new Headers();
+  // TODO: what is the difference between Accept and Content-Type?
   myHeaders.append("Accept", "application/json");
+  myHeaders.append("Content-Type", "application/json");
 
   return myHeaders;
 };
@@ -27,6 +29,15 @@ const configureRequest = (method, headers, data) => {
 };
 
 // TODO: create a base URL
+export const createUserNote = (userId, note) => {
+  const request = new Request(
+    `http://localhost:3000/api/users/${userId}/notes`
+  );
+  const init = configureRequest("POST", defaultHeaders(), note);
+
+  return fetch(request, init);
+};
+
 export const getUserNotes = userId => {
   const request = new Request(
     `http://localhost:3000/api/users/${userId}/notes`

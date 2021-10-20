c564a4895916e6a98dd23cb41ac481c8d3a9c7fe
Joshua Cole <9787792+jcole356@users.noreply.github.com>
2021-10-19 23:42:06 -0700

    Add tsx to eslintrc.js for jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "extends": ["airbnb", "plugin:prettier/recommended"],
-    "globals": {
-        "Headers": "readonly",
-        "Request": "readonly",
-        "document": "readonly",
-        "fetch": "readonly",
-        "sessionStorage": "readonly"
-    }
+  parser: "babel-eslint",
+  extends: ["airbnb", "plugin:prettier/recommended"],
+  globals: {
+    "Headers": "readonly",
+    "Request": "readonly",
+    "document": "readonly",
+    "fetch": "readonly",
+    "sessionStorage": "readonly"
+  },
+  rules:  {
+    'react/jsx-filename-extension': [2, { 'extensions': ['.jsx','.tsx'] }],
+  },
 };

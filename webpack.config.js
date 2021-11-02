81593f60845887f212e5c606b634b92927db515e
Joshua Cole <9787792+jcole356@users.noreply.github.com>
2021-11-01 17:27:04 -0700

    Upgrade babel (#49)
    * Upgrade babel-loader
    * Upgrade @babel/core
    * Upgrade @babel/preset-env
    * Remove @babel/plugin-proposal-class-properties
    * Upgrade @babel/preset-react

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,6 @@ module.exports = {
         exclude: /node_modules/,
         options: {
           presets: ["@babel/preset-env", "@babel/preset-react"],
-          plugins: ["@babel/plugin-proposal-class-properties"],
         },
       },
       {
